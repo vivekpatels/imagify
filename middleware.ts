0f88f2f1dfa5f1106a742c7cc035073dbec22e93
vivekkumar.p <vivekkumar.p@signdesk.com>
2024-02-25 21:16:52 +0530

    implement clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
 
   // Routes that can be accessed while signed out
-//   publicRoutes: ['/sign-in'],
+  publicRoutes: ['/api/webhooks/clerk'],
   // Routes that can always be accessed, and have
   // no authentication information
 //   ignoredRoutes: ['/no-auth-in-this-route'],
